edecf860e22831fca4c242a20dbd8ca8da743cc2
erwanFab <>
2015-03-03 13:08:32 +0100

    illustration

---
+++ ilustration.php
@@ -0,0 +1,24 @@
+<?php
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author erwanFab
+ */
+
+echo '<html><body>
+<div class = "cadre"></div>
+
+<div id = "panier"></div>
+<div id = "chien"></div>
+<div id = "garde"></div>
+<div id = "materiel"></div>
+
+
+</body></html>';
+?>
